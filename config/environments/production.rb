03eb44b6c7c443b4aa24e07fd7be8a8ff4d9ef26
matmenzl <mathias.menzl@gmail.com>
2015-04-12 22:42:08 +0200

    changed action-mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Required for Heroku
   # set this to the actual host
-  config.action_mailer.default_url_options = { :host => 'nachbarer.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'meinenachberer.ch' }
 
 
   # Do not dump schema after migrations.
