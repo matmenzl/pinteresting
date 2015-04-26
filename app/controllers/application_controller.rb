efeaaa45ffd68e2b5978054199ac57e90f7e60a9
Mathias Menzl <info@mathiasmenzl.ch>
2015-04-26 23:41:14 +0200

    Revert "i18n + users prefered locale"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
  before_action :configure_permitted_parameters, if: :devise_controller?
- before_action :set_locale
- 
+
  protected
 
   def configure_permitted_parameters
@@ -19,15 +18,5 @@ class ApplicationController < ActionController::Base
       flash[:notice] = "Sorry, but this pin isn't located in your area!"
       redirect_to root_path
     end
-  end
-
-  def set_locale
-    if cookies[:user_locale] && I18n.available_locales.include?(cookies[:user_locale].to_sym)
-      loc = cookies[:user_locale].to_sym
-    else
-      loc = http_accept_language.compatible_language_from(I18n.available_locales)
-      cookies.permanent[:user_locale] = loc
-    end
-    I18n.locale = loc
   end
 end
